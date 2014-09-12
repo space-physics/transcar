361c680ea6cdf32b63dfbb263a8783736b185c26
scienceopen <scienceopen@users.noreply.github.com>
2014-09-12 17:04:20 -0400

    parallel path tweaks

--- dir.transcar.server/run_beams_parallel.sh
+++ dir.transcar.server/run_beams_parallel.sh
@@ -15,5 +15,5 @@ tee -a "$1"
 
 export -f teea
 # jobs is equal to number of CPU cores by default
-parallel -S labHST0 -S labHST1 \
-    --eta --progress --joblog parallellog --colsep ',' beamRunner.sh :::: $BeamEnergyTableFN
+parallel -S labHST0 \
+    --eta --progress --joblog parallellog --colsep ',' transcar/dir.transcar.server/beamRunner.sh :::: $BeamEnergyTableFN

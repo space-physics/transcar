1e43fffc89cd9d2563b1be287ed76ab4419f6bf0
scienceopen <scienceopen@users.noreply.github.com>
2014-09-14 15:33:52 -0400

    comments, suppress excessive console text

--- dir.transcar.server/run_beams_parallel.sh
+++ dir.transcar.server/run_beams_parallel.sh
@@ -4,6 +4,18 @@
 # this script for loops transcar, making a new precinput.dat each time for the 
 # respective beam energies.
 # this program is meant to be run from dir.transcar.server directory- cd there first
+#
+# USAGE HINTS:
+# 0) because I don't yet use the rsync --transfer, --cleanup option of GNU parallel
+# you must ensure the remote nodes have the latest version of your code. I handle this
+# with git
+# 1) consider using ssh-add with -t more than long enough to complete your calculation, 
+# since a new SSH login is emitted for each beam! The simulation process will not
+# continue for new beams if your ssh-add -t has expired!
+# 2) when simulation is completed, I use SFTP to gather the results to my main 
+# analysis PC
+# so you see that I could skip steps 0 and 2 by using parallel rsync, but I haven't
+# tested this yet and wanted to avoid a mishap.
 
 BeamEnergyTableFN=BT_E1E2prev.csv
 

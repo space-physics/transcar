77bd65848f14dc723b5e281b0faa4e00089f8a2e
scienceopen <scienceopen@users.noreply.github.com>
2015-03-16 02:33:41 -0400

    allow proceeding if one beam fails

--- dir.transcar.server/beamRunner.sh
+++ dir.transcar.server/beamRunner.sh
@@ -21,8 +21,8 @@ flux0=70114000000.0 #FIXME where does this come from?
 tstart=$(grep "precipitation start time (seconds" $TCconfig | cut -f1)
 tfin=$(grep "precipitation end time (seconds" $TCconfig | cut -f1)
 
-[[ -z $tstart ]] && { echo "error: could not find precip start in $TCconfig"; exit 1; } || { echo "using tstart= $tstart"; }
-[[ -z $tfin ]] && { echo "error: could not find precip end in $TCconfig"; exit 1; }  || { echo "using tfin= $tfin"; }
+[[ -z $tstart ]] && { echo "error: could not find precip start in $TCconfig"; exit 1; } #|| { echo "using tstart= $tstart"; }
+[[ -z $tfin ]] && { echo "error: could not find precip end in $TCconfig"; exit 1; }  #|| { echo "using tfin= $tfin"; }
 
 # make a directory for this beam -- 
 # everything relevant to sim will reside in this directory, including executable!

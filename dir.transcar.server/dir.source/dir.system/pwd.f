bfab2e8046e0302cd08bda8ac177cb2b1ccd8f30
scienceopen <scienceopen@users.noreply.github.com>
2015-03-18 04:02:12 -0400

    can compile all with gfortran -O2

--- dir.transcar.server/dir.source/dir.system/pwd.f
+++ dir.transcar.server/dir.source/dir.system/pwd.f
@@ -1,33 +1,34 @@
-	subroutine pwd(file_dir,len_file,data_dir,len_data)
+	  subroutine pwd(file_dir,len_file,data_dir,len_data)
 
-	integer len_file,len_data
-	character*(*) file_dir,data_dir
-	character*5 archi,architecture
+	  integer len_file,len_data
+	  character*(*) file_dir,data_dir
+	  character*5 archi,architecture
 
-	call getcwd(file_dir)
-	len_file=lenc(file_dir)
-	if (file_dir(len_file:len_file).ne.'/') then
+	  call getcwd(file_dir)
+	  len_file=lenc(file_dir)
+	  if (file_dir(len_file:len_file).ne.'/') then
           len_file=len_file+1
           file_dir(len_file:len_file)='/'
-	endif
+	  endif
         data_dir=file_dir(1:len_file)//'dir.data/'
         len_data=lenc(data_dir)
         archi=architecture(data_dir(1:len_data))
         data_dir=data_dir(1:len_data)//'dir.'//archi
         len_data=lenc(data_dir)
-	data_dir=data_dir(1:len_data)//'/'
-	len_data=len_data+1
+	  data_dir=data_dir(1:len_data)//'/'
+	  len_data=len_data+1
 	
-	return
-	end
+	  return
+	  end
 
-	subroutine getcwd(file_dir)
+! MH this function is now built-in to both ifort and gfortran
+!	  subroutine getcwd(file_dir)
+!
+!	  integer*4 ilen,ierror
+!	  character*(*) file_dir
 
-	integer*4 ilen,ierror
-	character*(*) file_dir
-
-	call pxfgetcwd(file_dir,ilen,ierror)
+!	  call pxfgetcwd(file_dir,ilen,ierror)
 !	file_dir='/home/blelly/dir.spacegrid'
 !	file_dir=''
-	return
-	end
+!	  return
+!	  end

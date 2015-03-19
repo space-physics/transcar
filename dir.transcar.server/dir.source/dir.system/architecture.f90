fe28779d4b4cb6b4e5cf6295ba5faa0723143013
scienceopen <scienceopen@users.noreply.github.com>
2015-03-19 11:09:32 -0400

    revert

--- dir.transcar.server/dir.source/dir.system/architecture.f90
+++ dir.transcar.server/dir.source/dir.system/architecture.f90
@@ -5,7 +5,7 @@ function architecture(path)
     integer*1  ::  iarch
     integer*2  :: itest=1
 
-    open(99,file='systype',form='unformatted',status='unknown')
+    open(99,file=path//'/type',form='unformatted',status='unknown')
     write(99) itest
     rewind(99)
     read(99) iarch

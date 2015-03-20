0750a4ca37949b0eb78ffd794da1ba6b3600d652
nucl <hirsch617@gmail.com>
2015-03-20 02:11:41 -0400

    standards

--- dir.transcar.server/dir.source/dir.cine/quelle_grille.f
+++ dir.transcar.server/dir.source/dir.cine/quelle_grille.f
@@ -5,7 +5,7 @@ c
      &                          nang,nango2,gmu,gwt,angzb)
 
 c
- 	implicit none
+ 	    implicit none
 
         include 'TRANSPORT.INC'
 c
@@ -16,33 +16,33 @@ c 	Sorties : nombres d'energie, d'angles
 c 	grille d'energie (centre de grille, bas, largeur)
 c 	grille d'angle (angle, cosinus, poids)
 
- 	integer nen,nang,nango2
- 	integer i1,i2,i3
- 	real centE(nbren),botE(nbren),ddeng(nbren)
- 	real angzb(2*nbrango2),gmu(2*nbrango2),gwt(2*nbrango2)
+     	integer nen,nang,nango2
+     	integer i1,i2,i3
+     	real centE(nbren),botE(nbren),ddeng(nbren)
+     	real angzb(2*nbrango2),gmu(2*nbrango2),gwt(2*nbrango2)
 c
 c 	Parametres internes :
- 	real spfac,emax,emin	   !calcul eventuel d'une grille propre
- 	integer ien,iang,iost
+     	real spfac,emax,emin	   !calcul eventuel d'une grille propre
+     	integer ien,iang,iost
 c 	Pour transferer via DATDEG les caracteristiques de grille a 
 c 	degrad.f
- 	integer type_de_grille,lenc,idess(2),iprint1,iprint2,iprint3,
-     .		iprint4
- 	logical logint,lt1,lt2,lt3,lt4,lopal
+ 	    integer type_de_grille,lenc,idess(2),iprint1,iprint2,iprint3,
+     &		iprint4
+ 	    logical logint,lt1,lt2,lt3,lt4,lopal
         character*80 crsin,crs,rdt
 c
- 	real pideg
+ 	    real pideg
         data pideg/57.29578/
 c
 c 	On stoke les parametres de calcul de degrad.
         open(fic_datdeg,file=data_path(1:lpath_data)
      &                             //'dir.cine/DATDEG')
-	rewind(fic_datdeg)
- 	read(fic_datdeg,*)type_de_grille
- 	if(type_de_grille .eq.0) then
- 	  close(fic_datdeg)
- 	  return
- 	endif
+        rewind(fic_datdeg)
+     	read(fic_datdeg,*)type_de_grille
+     	if(type_de_grille .eq.0) then
+     	  close(fic_datdeg)
+     	  return
+     	endif
         read(fic_datdeg,1000)crsin
         crsin = crsin(1:lenc(crsin))
         read(fic_datdeg,1000) crs
@@ -59,14 +59,14 @@ c 	On stoke les parametres de calcul de degrad.
         read(fic_datdeg,1000)lt3
         read(fic_datdeg,1000)lt4
         read(fic_datdeg,1000)lopal
- 	close(fic_datdeg)
+ 	    close(fic_datdeg)
 c
 
 !-MZ
         emax=70000.
 !-MZ
 
- 	if (type_de_grille.eq.1)then
+ 	    if (type_de_grille.eq.1)then
 c 	  On utilise les grilles detaillees
  	  if (emax.le.355.8)then
  	    crs = 'dir.cine/dir.seff/crsa1'
@@ -106,7 +106,8 @@ c           Calcul des energies de bas de grille
               botE(ien+1) = botE(ien)+ddeng(ien+1)
             enddo
  	  endif
- 	else if (type_de_grille.eq.2)then
+ 	  
+ 	  else if (type_de_grille.eq.2)then
  	  if (emax.le.355.8)then
  	    crs = 'dir.cine/dir.seff/crsb1'
  	    rdt = 'dir.cine/dir.seff/rdtb1'
@@ -145,13 +146,13 @@ c           Calcul des energies de bas de grille         :
               botE(ien+1) = botE(ien)+ddeng(ien+1)
             enddo
  	  endif
- 	endif
+ 	  endif
 c
 c       Lecture des grilles d'energie.
 c	goto 314
         open(icrsin,file=data_path(1:lpath_data)
      &                         //crs,status='OLD',form='UNFORMATTED',
-     .          iostat=iost,err=992)
+     &          iostat=iost,err=992)
         rewind icrsin
         read(icrsin) nen,i1,i2,i3
         read(icrsin) (centE(ien),ien=1,nen)
@@ -176,67 +177,67 @@ c 	On previent maintenant degrad de ou il faut lire les sections
 c 	efficaces.
         open(fic_datdeg,file= data_path(1:lpath_data)
      &                              //'dir.cine/DATDEG')
-	rewind(fic_datdeg)
- 	write(fic_datdeg,1010)type_de_grille
+        rewind(fic_datdeg)
+     	write(fic_datdeg,1010)type_de_grille
         write(fic_datdeg,1020) crsin
         write(fic_datdeg,1030) crs
         write(fic_datdeg,1040) rdt
         write(fic_datdeg,1050) idess(1),idess(2)
         write(fic_datdeg,1060) iprint1,iprint2,iprint3,iprint4
 
- 	if(logint)then
+ 	    if(logint)then
           write(fic_datdeg,1070)
- 	else
+ 	    else
           write(fic_datdeg,1075)
- 	endif
+ 	    endif
 1070 	format('.true',11x,
      &    'logint: interpolation type for cross sections')
 1075 	format('.false.',10x,
      &    'logint: interpolation type for cross sections')
 c
- 	if(lt1)then
+     	if(lt1)then
           write(fic_datdeg,1080)
- 	else
+     	else
           write(fic_datdeg,1085)
- 	endif
+     	endif
 1080 	format('.true.',11x,'lt1   : test without ionization')
 1085 	format('.false.',10x,'lt1   : test without ionization')
 
- 	if(lt2)then
-          write(fic_datdeg,1090)
- 	else
-          write(fic_datdeg,1095)
- 	endif
+     	if(lt2)then
+              write(fic_datdeg,1090)
+     	else
+              write(fic_datdeg,1095)
+     	endif
 1090 	format('.true.',11x,'lt2   : test only with ionization')
 1095 	format('.false.',10x,'lt2   : test only with ionization')
 
- 	if(lt3)then
-          write(fic_datdeg,1100)
- 	else
-          write(fic_datdeg,1105)
- 	endif
+     	if(lt3)then
+              write(fic_datdeg,1100)
+     	else
+              write(fic_datdeg,1105)
+     	endif
 1100 	format('.true.',11x,'lt3   : test without degradation')
 1105 	format('.false.',10x,'lt3   : test without degradation')
 
- 	if(lt4)then
-          write(fic_datdeg,1110)
- 	else
-          write(fic_datdeg,1115)
- 	endif
+     	if(lt4)then
+              write(fic_datdeg,1110)
+     	else
+              write(fic_datdeg,1115)
+     	endif
 1110 	format('.true.',11x,'lt4   : test without excitation')
 1115 	format('.false.',10x,'lt4   : test without excitation')
 
- 	if(lopal)then
-          write(fic_datdeg,1120)
- 	else
-          write(fic_datdeg,1125)
- 	endif
+     	if(lopal)then
+              write(fic_datdeg,1120)
+     	else
+              write(fic_datdeg,1125)
+     	endif
 1120 	format('.true.',11x,
      &    'lopal : sec. distrib. de Opal (Rees sinon)')
 1125 	format('.false.',10x,
      &    'lopal : sec. distrib. de Opal (Rees sinon)')
 
- 	close(fic_datdeg)
+ 	  close(fic_datdeg)
 c
 1000    format(a)
 1010 	format(i2,10x,' Pour iniflu : 1 si Egrid detaillee, 2 sinon')
@@ -255,32 +256,32 @@ c1040 	format(a,3x,' Fic. de sortie des seff diff. (Eprim-->E)')
      &    8x,'sigel  siginel  rmatrix diff-sig-test ',/,4i10)
 1130 	format(a7)
 c
-	nang=8
-	nango2=nang/2
-	  
-	gmu(1)= .9305681586266
-	gmu(2)= .6699905395508
-	gmu(3)= .3300094604492
-	gmu(4)= .0694318413734
-	gmu(5)=-.0694318413734
-	gmu(6)=-.3300094604492
-	gmu(7)=-.6699905395508
-	gmu(8)=-.9305681586266
+        nang=8
+        nango2=nang/2
+          
+        gmu(1)= .9305681586266
+        gmu(2)= .6699905395508
+        gmu(3)= .3300094604492
+        gmu(4)= .0694318413734
+        gmu(5)=-.0694318413734
+        gmu(6)=-.3300094604492
+        gmu(7)=-.6699905395508
+        gmu(8)=-.9305681586266
 
-	gwt(1)=.1739274263382
-	gwt(2)=.3260725736618
-	gwt(3)=.3260725736618
-	gwt(4)=.1739274263382
-	gwt(5)=.1739274263382
-	gwt(6)=.3260725736618
-	gwt(7)=.3260725736618
-	gwt(8)=.1739274263382
+        gwt(1)=.1739274263382
+        gwt(2)=.3260725736618
+        gwt(3)=.3260725736618
+        gwt(4)=.1739274263382
+        gwt(5)=.1739274263382
+        gwt(6)=.3260725736618
+        gwt(7)=.3260725736618
+        gwt(8)=.1739274263382
 
-	do iang=1,nang
-	  angzb(iang)=pideg*acos(gmu(iang))
-	enddo
+        do iang=1,nang
+          angzb(iang)=pideg*acos(gmu(iang))
+        enddo
 c
- 	return
+ 	    return
 992     print*,' Cross-section file is in error. Status=',iost
- 	stop
-	end
+     	stop
+        end

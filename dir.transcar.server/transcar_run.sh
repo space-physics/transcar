1f5281b6c67b2a153ed68a526afbfaf7791f7999
scienceopen <scienceopen@users.noreply.github.com>
2015-03-29 23:09:19 -0400

    functionalize

--- dir.transcar.server/transcar_run.sh
+++ dir.transcar.server/transcar_run.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 # brought up to BASH 4 conventions by Michael Hirsch mhirsch@bu.edu
-# original by Matt Zettergren 
+# original by Matt Zettergren
 
 # note: relative paths used here work because of "cd" step in beamRunner.sh
 
-runTranscar()
+copyTranscarFiles()
 {
-[[ -z $1 ]] && { echo "error: must specify root directory e.g. ./run_beams ../BT"; exit 1; } 
-
-#set +e #don't stop on error unless I direct it to
-
-#not necessary
-#http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in 
-#TDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" 
-
+[[ -z $1 ]] && { echo "error: must specify root directory"; exit 1; }
 ODIR=$1
-TClog=$ODIR/transcarBash.log
-TCconfig=dir.input/DATCAR
-msisinitfn=$(grep "input file (initial ionospheric conditions)" $TCconfig | cut -d" " -f1)
-echo "using msis init file $msisinitfn as specified in $TCconfig">>$TClog 2>&1
+[[ ! -d $ODIR ]] && { echo "Beam dir. $ODIR does not exist" | tee -a $TClog; exit 98; }
 
-#check for unused output dir
-[[ ! -d "$ODIR" ]] && { echo "Energy directory $ODIR does not exist" | tee -a $TClog; exit 98; }
+TClog=$ODIR/transcarBash.log
+TCconfig=dir.input/DATCAR #this is hard-coded in transcar
+
+initfn=$(grep "input file (initial ionospheric conditions)" $TCconfig | cut -d" " -f1)
+if [[ -n $initfn ]]; then
+ echo "using msis init file $initfn as specified in $TCconfig">>$TClog 2>&1
+else
+ echo "initial conditions file not found, aborting"
+ exit 1
+fi
 
 #copy transcar input
-cp -v -t "$ODIR/dir.input/" $TCconfig dir.input/$msisinitfn >>$TClog 2>&1
+cp -v -t "$ODIR/dir.input/" $TCconfig dir.input/$initfn >>$TClog 2>&1
 cp -v -t "$ODIR/dir.data/" dir.data/type >>$TClog 2>&1
 cp -v -t "$ODIR/dir.data/dir.linux/dir.geomag/" dir.data/dir.linux/dir.geomag/{data_geom.bin,igrf90.dat,igrf90s.dat} >>$TClog 2>&1
 cp -v -t "$ODIR/dir.data/dir.linux/dir.projection/" dir.data/dir.linux/dir.projection/varpot.dat >>$TClog 2>&1
@@ -35,23 +33,22 @@ cp -v -t "$ODIR/dir.data/dir.linux/dir.cine/dir.seff/" dir.data/dir.linux/dir.ci
 
 #copy transcar itself
 cp -v transconvec_13.op.out "$ODIR/"
+}
 
-
-# run TRANSCAR
-#echo "RUN_TRANSCAR.SH: transconvec_13.op is running"
+runTranscar()
+{
 #time strace -e trace=open transconvec_13.op.out | tee $PWD/bid
 #time transconvec_13.op.out | tee $PWD/bid | egrep -A3 -e "*******|Error|felin.f"
 #time transconvec_13.op.out > $PWD/bid & tail -f $PWD/bid | egrep --line-buffered -A3 -e "*******|Error|felin.f"
 
-#must have /usr/bin/time to use the system time command, as bash time command won't accept arguments!
-# egrep --line-buffered is necessary to avoid annoying random line truncations while waiting for stdout to fill!
+#must have /usr/bin/time to use the system time command, as bash time command won't accept arguments
+# egrep --line-buffered is necessary to avoid annoying random line truncations while waiting for stdout to fill
 #/usr/bin/time --output="$ODIR/timing.txt" ./transconvec_13.op.out | \
-
 
 # exec used to reduce memory usage http://stackoverflow.com/questions/786376
  (cd $ODIR && exec ./transconvec_13.op.out) >>$ODIR/TranscarErrors.txt 2>&1
     # egrep -i --line-buffered -e "Warning|Error|felin.f|trans.f|Input eV/cm2/s" | \
-      
+
 }
 
 # this noMore is only used for non-parallel runs
@@ -62,5 +59,6 @@ cp -v transconvec_13.op.out "$ODIR/"
 #}
 #trap noMore SIGINT
 
-runTranscar $1
+copyTranscarFiles $1
+runTranscar
 

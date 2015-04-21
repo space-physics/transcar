58b4efadb0b6dcff4bfa521494797aa6d54aeecd
scivision <scivision@users.noreply.github.com>
2015-04-21 10:43:52 -0600

    copy FELTRANS

--- dir.transcar.server/transcar_run.py
+++ dir.transcar.server/transcar_run.py
@@ -26,8 +26,8 @@ def runTranscar(odir,errfn,msgfn):
             out,err = proc.communicate() #this makes popen block (we want this)
             #print(out,err) #will be none since we didn't use PIPE
         except IOError as e:
-            exit(e)   
- 
+            exit(e)
+
 def transcaroutcheck(odir,errfn):
     fok = join(odir,fileok)
     try:
@@ -42,42 +42,42 @@ def transcaroutcheck(odir,errfn):
                 f.write('false')
     except IOError as e:
         print('transcaroutcheck: problem reading transcar output.  {}'.format(e) )
-                
+
 def setuptranscario(rodir,beamEnergy):
-    inp = readTranscarInput(datcar) 
-    
+    inp = readTranscarInput(datcar)
+
     odir = join(rodir,'beam{:.1f}'.format(beamEnergy))
-    
-    try: 
+
+    try:
         makedirs(join(odir,'dir.output'))
-    except OSError: 
+    except OSError:
         exit('{} directory already existed, aborting'.format(beamEnergy))
-    
+
     flist = [datcar, join('dir.input',inp['precfile']), 'dir.data/type',transcarexe]
     flist.extend(['dir.data/dir.linux/dir.geomag/' +s  for s in ['data_geom.bin','igrf90.dat','igrf90s.dat']])
     flist.append('dir.data/dir.linux/dir.projection/varpot.dat')
-    flist.extend(['dir.data/dir.linux/dir.cine/' +s  for s in ['DATDEG','DATFEL','DATTRANS','flux.flag']])
+    #transcar sigsegv on val_fit_ if FELTRANS is blank!
+    flist.extend(['dir.data/dir.linux/dir.cine/' +s  for s in ['DATDEG','DATFEL','DATTRANS','flux.flag','FELTRANS']])
     flist.append('dir.data/dir.linux/dir.cine/dir.euvac/EUVAC.dat')
     flist.extend(['dir.data/dir.linux/dir.cine/dir.seff/' +s  for s in ['crsb8','crsphot1.dat','rdtb8']])
     cp_parents(flist,odir)
-    
-    empty_file(join(odir,'dir.data/dir.linux/dir.cine/FELTRANS'))
-    
+
+
     return inp,odir
-        
+
 def setupPrecipitation(odir,inp,E1,E2,pr1,pr2, flux0):
     dE = E2-E1
     Esum = E2+E1
     flux = flux0 / 0.5 / Esum / dE
     Elow = E1 - 0.5*(E1 - pr1)
     Ehigh =E2 - 0.5*(E2 - pr2)
-    
+
     precout = '\n'.join(('{}','{} {}','{} -1.0','{}','-1.0 -1.0')).format(
               inp['precipstartsec'], Elow, flux, Ehigh, inp['precipendsec'])
-    
+
     with open(join(odir,precfn),'w') as f:
         f.write(precout)
-#%%    
+#%%
 if __name__ == '__main__':
     from argparse import ArgumentParser
     p = ArgumentParser(description='parallel instance transcar runner')
@@ -93,8 +93,7 @@ if __name__ == '__main__':
 
     datinp,odir = setuptranscario(p.rodir,p.E1)
     setupPrecipitation(odir,datinp, p.E1, p.E2, p.pr1, p.pr2, p.flux0)
-    
+
     runTranscar(odir, p.errfn, p.msgfn)
 #%% check output trivially
-    transcaroutcheck(odir,p.errfn) 
-    
+    transcaroutcheck(odir,p.errfn)

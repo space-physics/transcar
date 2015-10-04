b1936cb16fb1350aeab08f97fdb18313b445dee9
scienceopen <scienceopen@users.noreply.github.com>
2015-10-04 11:26:01 -0400

    more robust relative data path names

--- transcar/dir.source/dir.cine/quelle_grille.f
+++ transcar/dir.source/dir.cine/quelle_grille.f
@@ -20,130 +20,139 @@ c
 c 	Parametres internes :
      	real spfac,emax,emin	   !calcul eventuel d'une grille propre
      	integer ien,iang,iost
-c 	Pour transferer via DATDEG les caracteristiques de grille a 
+      character(len=80),parameter ::
+     &        datdegfn = 'dir.data/dir.linux/dir.cine/DATDEG'
+
+
+
+c 	Pour transferer via DATDEG les caracteristiques de grille a
 c 	degrad.f
  	    integer type_de_grille,lenc,idess(2),iprint1,iprint2,iprint3,
      &		iprint4
  	    logical logint,lt1,lt2,lt3,lt4,lopal
         character*80 crsin,crs,rdt,crsfn
 c
- 	    real pideg
-        data pideg/57.29578/
+ 	    real,parameter :: pideg=57.29578
 c
 c 	On stoke les parametres de calcul de degrad.
-        open(fic_datdeg,file='dir.data/dir.linux/dir.cine/DATDEG',
-     &       status='old')
+        open(fic_datdeg, file=datdegfn, status='old')
 
         rewind(fic_datdeg)
      	read(fic_datdeg,*) type_de_grille
      	if(type_de_grille .eq.0) then
+        print*,'DATDEG specifies grid type 0'
      	  close(fic_datdeg)
      	  return
      	endif
-        read(fic_datdeg,1000)crsin
+        read(fic_datdeg,1000) crsin
         crsin = crsin(1:lenc(crsin))
         read(fic_datdeg,1000) crs
         crs = crs(1:lenc(crs))
         read(fic_datdeg,1000) rdt
         rdt = rdt(1:lenc(rdt))
-        call xline(4,fic_datdeg)
+        call xline(4,fic_datdeg) !skip 4 lines
         read (fic_datdeg,*) idess(1),idess(2)
-        call xline(2,fic_datdeg)
+        call xline(2,fic_datdeg) !skip 2 lines
         read (fic_datdeg,*) iprint1,iprint2,iprint3,iprint4
-        read(fic_datdeg,1000)logint
-        read(fic_datdeg,1000)lt1
-        read(fic_datdeg,1000)lt2
-        read(fic_datdeg,1000)lt3
+        read(fic_datdeg,1000) logint
+        read(fic_datdeg,1000) lt1
+        read(fic_datdeg,1000) lt2
+        read(fic_datdeg,1000) lt3
         read(fic_datdeg,1000)lt4
         read(fic_datdeg,1000)lopal
  	    close(fic_datdeg)
-c
+
 
 !-MZ
         emax=70000.
 !-MZ
 
- 	    if (type_de_grille.eq.1)then
+ 	   if (type_de_grille.eq.1)then
 c 	  On utilise les grilles detaillees
- 	  if (emax.le.355.8)then
- 	    crs = 'dir.cine/dir.seff/crsa1'
- 	    rdt = 'dir.cine/dir.seff/rdta1'
- 	  else if (emax.gt.355.8 .and. emax.le.720.6)then
- 	    crs = 'dir.cine/dir.seff/crsa2'
- 	    rdt = 'dir.cine/dir.seff/rdta2'
- 	  else if (emax.gt.720.6 .and. emax.le.2193.)then
- 	    crs = 'dir.cine/dir.seff/crsa3'
- 	    rdt = 'dir.cine/dir.seff/rdta3'
- 	  else if (emax.gt.2193. .and. emax.le.4394.)then
- 	    crs = 'dir.cine/dir.seff/crsa4'
- 	    rdt = 'dir.cine/dir.seff/rdta4'
- 	  else if (emax.gt.4394. .and. emax.le.7057.)then
- 	    crs = 'dir.cine/dir.seff/crsa5'
- 	    rdt = 'dir.cine/dir.seff/rdta5'
- 	  else if (emax.gt.7057. .and. emax.le.21700.)then
- 	    crs = 'dir.cine/dir.seff/crsa6'
- 	    rdt = 'dir.cine/dir.seff/rdta6'
- 	  else if (emax.gt.21700. .and. emax.le.43410.)then
- 	    crs = 'dir.cine/dir.seff/crsa7'
- 	    rdt = 'dir.cine/dir.seff/rdta7'
- 	  else if (emax.gt.43410. .and. emax.le.70440.)then
- 	    crs = 'dir.cine/dir.seff/crsa8'
- 	    rdt = 'dir.cine/dir.seff/rdta8'
- 	  else 
- 	    nen = 400
- 	    crs = 'dir.cine/dir.seff/crsa'
- 	    rdt = 'dir.cine/dir.seff/rdta'
- 	    emin = 1.e-01
-c 	    On ne va pas au dessus de 140 keV...
- 	    emax = min(emax,140000.0)
+!     "use detailed grid"
+   	  if (emax.le.355.8)then
+   	    crs = 'dir.cine/dir.seff/crsa1'
+   	    rdt = 'dir.cine/dir.seff/rdta1'
+ 	    else if (emax.gt.355.8 .and. emax.le.720.6)then
+   	    crs = 'dir.cine/dir.seff/crsa2'
+   	    rdt = 'dir.cine/dir.seff/rdta2'
+ 	    else if (emax.gt.720.6 .and. emax.le.2193.)then
+   	    crs = 'dir.cine/dir.seff/crsa3'
+   	    rdt = 'dir.cine/dir.seff/rdta3'
+ 	    else if (emax.gt.2193. .and. emax.le.4394.)then
+   	    crs = 'dir.cine/dir.seff/crsa4'
+   	    rdt = 'dir.cine/dir.seff/rdta4'
+ 	    else if (emax.gt.4394. .and. emax.le.7057.)then
+   	    crs = 'dir.cine/dir.seff/crsa5'
+   	    rdt = 'dir.cine/dir.seff/rdta5'
+ 	    else if (emax.gt.7057. .and. emax.le.21700.)then
+   	    crs = 'dir.cine/dir.seff/crsa6'
+   	    rdt = 'dir.cine/dir.seff/rdta6'
+ 	    else if (emax.gt.21700. .and. emax.le.43410.)then
+   	    crs = 'dir.cine/dir.seff/crsa7'
+   	    rdt = 'dir.cine/dir.seff/rdta7'
+ 	    else if (emax.gt.43410. .and. emax.le.70440.)then
+   	    crs = 'dir.cine/dir.seff/crsa8'
+   	    rdt = 'dir.cine/dir.seff/rdta8'
+ 	    else
+   	    nen = 400
+   	    crs = 'dir.cine/dir.seff/crsa'
+   	    rdt = 'dir.cine/dir.seff/rdta'
+   	    emin = 1.e-01
+!	    On ne va pas au dessus de 140 keV...
+!     "do not go above 140keV (?) "
+   	    emax = min(emax,140000.0)
+              print*,'call gridpolo'
             call gridpolo (nen,emin,emax,centE,ddeng,spfac)
 c           Calcul des energies de bas de grille
             botE(1) = max(centE(1) - ddeng(1)/2.,1.e-03)
             do ien = 1,nen-1
               botE(ien+1) = botE(ien)+ddeng(ien+1)
             enddo
- 	  endif
- 	  
+      print*,'using detail grid type 1, crs,rdt: ',crs,rdt
+ 	    endif
+
  	  else if (type_de_grille.eq.2)then
- 	  if (emax.le.355.8)then
- 	    crs = 'dir.cine/dir.seff/crsb1'
- 	    rdt = 'dir.cine/dir.seff/rdtb1'
- 	  else if (emax.gt.355.8 .and. emax.le.720.6)then
- 	    crs = 'dir.cine/dir.seff/crsb2'
- 	    rdt = 'dir.cine/dir.seff/rdtb2'
- 	  else if (emax.gt.720.6 .and. emax.le.2193.)then
- 	    crs = 'dir.cine/dir.seff/crsb3'
- 	    rdt = 'dir.cine/dir.seff/rdtb3'
- 	  else if (emax.gt.2193. .and. emax.le.4394.)then
- 	    crs = 'dir.cine/dir.seff/crsb4'
- 	    rdt = 'dir.cine/dir.seff/rdtb4'
- 	  else if (emax.gt.4394. .and. emax.le.7057.)then
- 	    crs = 'dir.cine/dir.seff/crsb5'
- 	    rdt = 'dir.cine/dir.seff/rdtb5'
- 	  else if (emax.gt.7057. .and. emax.le.21700.)then
- 	    crs = 'dir.cine/dir.seff/crsb6'
- 	    rdt = 'dir.cine/dir.seff/rdtb6'
- 	  else if (emax.gt.21700. .and. emax.le.43410.)then
- 	    crs = 'dir.cine/dir.seff/crsb7'
- 	    rdt = 'dir.cine/dir.seff/rdtb7'
- 	  else if (emax.gt.43410. .and. emax.le.70440.)then
- 	    crs = 'dir.cine/dir.seff/crsb8'
- 	    rdt = 'dir.cine/dir.seff/rdtb8'
- 	  else 
- 	    nen = 150
- 	    crs = 'dir.cine/dir.seff/crsb'
- 	    rdt = 'dir.cine/dir.seff/rdtb'
- 	    emin = 1.e-01
+   	  if (emax.le.355.8)then
+   	    crs = 'dir.cine/dir.seff/crsb1'
+   	    rdt = 'dir.cine/dir.seff/rdtb1'
+   	  else if (emax.gt.355.8 .and. emax.le.720.6)then
+   	    crs = 'dir.cine/dir.seff/crsb2'
+   	    rdt = 'dir.cine/dir.seff/rdtb2'
+   	  else if (emax.gt.720.6 .and. emax.le.2193.)then
+   	    crs = 'dir.cine/dir.seff/crsb3'
+   	    rdt = 'dir.cine/dir.seff/rdtb3'
+   	  else if (emax.gt.2193. .and. emax.le.4394.)then
+   	    crs = 'dir.cine/dir.seff/crsb4'
+   	    rdt = 'dir.cine/dir.seff/rdtb4'
+   	  else if (emax.gt.4394. .and. emax.le.7057.)then
+   	    crs = 'dir.cine/dir.seff/crsb5'
+   	    rdt = 'dir.cine/dir.seff/rdtb5'
+   	  else if (emax.gt.7057. .and. emax.le.21700.)then
+   	    crs = 'dir.cine/dir.seff/crsb6'
+   	    rdt = 'dir.cine/dir.seff/rdtb6'
+   	  else if (emax.gt.21700. .and. emax.le.43410.)then
+   	    crs = 'dir.cine/dir.seff/crsb7'
+   	    rdt = 'dir.cine/dir.seff/rdtb7'
+   	  else if (emax.gt.43410. .and. emax.le.70440.)then
+   	    crs = 'dir.cine/dir.seff/crsb8'
+   	    rdt = 'dir.cine/dir.seff/rdtb8'
+   	  else
+   	    nen = 150
+   	    crs = 'dir.cine/dir.seff/crsb'
+   	    rdt = 'dir.cine/dir.seff/rdtb'
+   	    emin = 1.e-01
 c 	    On ne va pas au dessus de 140 keV...
- 	    emax = min(emax,140000.0)
-            call gridpolo (nen,emin,emax,centE,ddeng,spfac)
+   	    emax = min(emax,140000.0)
+              call gridpolo (nen,emin,emax,centE,ddeng,spfac)
 c           Calcul des energies de bas de grille         :
-            botE(1) = max(centE(1) - ddeng(1)/2.,1.e-03)
-            do ien = 1,nen-1
-              botE(ien+1) = botE(ien)+ddeng(ien+1)
-            enddo
- 	  endif
- 	  endif
+              botE(1) = max(centE(1) - ddeng(1)/2.,1.e-03)
+              do ien = 1,nen-1
+                botE(ien+1) = botE(ien)+ddeng(ien+1)
+              enddo
+   	   endif
+      print*,'using detail grid type 2, crs,rdt: ',crs,rdt
+ 	    endif
 c
 c       Lecture des grilles d'energie.
 c	goto 314
@@ -174,10 +183,11 @@ c            enddo
 c
 c 	On previent maintenant degrad de ou il faut lire les sections
 c 	efficaces.
-        print*,'attempting to open DATDEG file'
-        open(fic_datdeg,file='/dir.data/dir.linux/dir.cine/DATDEG',
-     &       status='replace',err=993)
+        print*,'attempting to open ',datdegfn
+        open(fic_datdeg,file=datdegfn,
+     &       status='unknown',err=993) !yes unknown in case not all values rewritten
 !        rewind(fic_datdeg)
+       print*,'beginning to rewrite',datdegfn
      	write(fic_datdeg,1010)type_de_grille
         write(fic_datdeg,1020) crsin
         write(fic_datdeg,1030) crs
@@ -240,7 +250,7 @@ c
  	  close(fic_datdeg)
 c
 1000    format(a)
-1010 	format(i2,10x,' Pour iniflu : 1 si Egrid detaillee, 2 sinon')
+1010 	format(i2,10x,' for iniflu : 1 use detailed grid, 2 coarse grid')
 c1020 	format(a,3x,' Fichier d''entree des sections efficaces')
 c1030 	format(a,3x,' Fichier de sortie des seff(E)')
 c1040 	format(a,3x,' Fic. de sortie des seff diff. (Eprim-->E)')
@@ -258,7 +268,7 @@ c1040 	format(a,3x,' Fic. de sortie des seff diff. (Eprim-->E)')
 c
         nang=8
         nango2=nang/2
-          
+
         gmu(1)= .9305681586266
         gmu(2)= .6699905395508
         gmu(3)= .3300094604492

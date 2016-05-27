913cd36e7600dfbf9cb9cd73e8920b1d64bf7369
scienceopen <scienceopen@users.noreply.github.com>
2016-05-27 16:44:41 -0400

    cleanup, line truncation

--- transcar/transcar_run.py
+++ transcar/transcar_run.py
@@ -1,6 +1,8 @@
-#!/usr/bin/env python3
-from __future__ import division,absolute_import
-from pathdir2 import Path
+#!/usr/bin/env python
+"""
+This file is normally called from GNU Parallel
+"""
+from pathlib import Path
 import logging
 from collections import deque
 from subprocess import Popen

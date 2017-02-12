4874ec0726bf28c7997ea032c1d8551100df478d
michael <hirsch617@gmail.com>
2017-02-12 02:34:12 -0500

    pythonic

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 from setuptools import setup
-try:
-    import conda.cli
-    conda.cli.main('install','--file','requirements.txt')
-except Exception as e:
-    print(e)
 
+req=['h5py','scipy','pandas','pytz','numpy','nose','astropy','scikit-image','python-dateutil','matplotlib',
+    'histutils','transcarread']
 
 setup(name='transcar',
-      dependency_links = ['https://github.com/scienceopen/histutils/tarball/master#egg=histutils',
-                          'https://github.com/scienceopen/transcarread/tarball/master#egg=transcarread'],
-     install_requires=['histutils','transcarread'],
       packages=['transcar'],
+      url='https://github.com/scienceopen/transcar',
+      dependency_links = ['https://github.com/scienceopen/transcarread/tarball/master#egg=transcarread'],
+     install_requires=req,
 	  )
 	  

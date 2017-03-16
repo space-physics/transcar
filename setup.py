ac3b4d7307660fff1bb01a83fddd3d7a4aa575f5
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-03-16 01:48:58 -0400

    x

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ req=['h5py','scipy','pandas','pytz','numpy','nose','astropy','scikit-image','pyt
 
 setup(name='transcar',
       packages=['transcar'],
-      url='https://github.com/scienceopen/transcar',
-      dependency_links = ['https://github.com/scienceopen/transcarread/tarball/master#egg=transcarread'],
+      url='https://github.com/scivision/transcar',
+      dependency_links = ['https://github.com/scivision/transcarread/tarball/master#egg=transcarread'],
      install_requires=req,
 	  )
 	  
